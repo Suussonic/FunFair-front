1a67a3c4add48e89ef564908ce301ce84008317f
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-21 20:37:39 +0200

    -theme

--- PHP/loginForm.php
+++ PHP/loginForm.php
@@ -60,7 +60,6 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="shortcut icon" href="../ASSET/CARDBINDEX V5.png" type="image/x-icon">
     <link rel="stylesheet" href="../CSS/LoginForm.css">
-    <?php include 'theme.php'; ?>
     <title>Connexion</title>
 </head>
 <body>
