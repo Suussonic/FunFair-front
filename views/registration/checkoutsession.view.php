46ee33dc656133d1eb677827ab64dc152e38620d
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-24 20:26:02 +0200

    chekout session

--- views/registration/checkoutsession.view.php
+++ views/registration/checkoutsession.view.php
@@ -20,3 +20,31 @@ $checkout_session = $stripe->checkout->sessions->create([
 ]);
 
   echo json_encode(array('clientSecret' => $checkout_session->client_secret));
+
+  
+  $YOUR_DOMAIN = 'https://funfair.ovh';
+  $quantity = $_POST['poeple'];
+  try {
+      
+      $prices = \Stripe\Price::all([
+          'lookup_keys' => [$_POST['lookup_key']],
+          'expand' => ['data.product']
+      ]);
+  
+      $checkout_session = \Stripe\Checkout\Session::create([
+          'line_items' => [[
+              'price' => $prices->data[0]->id,
+              'quantity' => $quantity,
+          ]],
+  
+          'mode' => 'payment',
+          'success_url' => $YOUR_DOMAIN . '/php/payment/success.php?q='.$quantity.'&i='.$_POST['lookup_key'].'&p='.$prices->data[0]['unit_amount'],
+          'cancel_url' => $YOUR_DOMAIN . '/php/index.php',
+      ]);
+      header("HTTP/1.1 303 See Other");
+      header("Location: " . $checkout_session->url);
+  } catch (Error $e) {
+      http_response_code(500);
+      echo json_encode(['error' => $e->getMessage()]);
+  
+  }
