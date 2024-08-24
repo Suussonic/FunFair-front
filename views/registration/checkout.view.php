121f13ae2fea80f55fc325b52d80193c77f7bf0a
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-24 20:31:30 +0200

    autre test

--- views/registration/checkout.view.php
+++ views/registration/checkout.view.php
@@ -1,4 +1,40 @@
+<?php
 
+include_once('models/Database.php');
+require_once('../stripe/init.php');
+
+use Stripe\Stripe;
+
+$stripeSecretKey = 'sk_test_51PrNEeIpuwwZKBGaVhN6YsIQeDn7BQ8CEaBNCLrB9Iw8IFX0Q5yNe6Mssxb5khU4FG4jsm6TWvOWWWdqmIBtsrm700gE3NazR4';
+
+$stripe = new \Stripe\StripeClient($stripeSecretKey);
+header('Content-Type: application/json');
+
+$idstripe = $_POST['idstripe'];
+
+function getNom($idstripe):string{
+  $query = $dbh -> prepare("Select nom from attractions where idstripe = :idstripe");
+  $query -> bindParam(':idstripe', $idstripe);
+  $query -> execute();
+  $result = $query -> fetch();
+  return $result['nom'];
+}
+
+$YOUR_DOMAIN = 'http://localhost:4242';
+
+$checkout_session = $stripe->checkout->sessions->create([
+  'ui_mode' => 'embedded',
+  'line_items' => [[
+    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+    'price' => '{{PRICE_ID}}',
+    'quantity' => 1,
+  ]],
+  'mode' => 'payment',
+  'return_url' => $YOUR_DOMAIN . '/return.html?session_id={CHECKOUT_SESSION_ID}',
+]);
+
+  echo json_encode(array('clientSecret' => $checkout_session->client_secret));
+  ?>
   <!DOCTYPE html>
   <html lang="fr">
   
