35e39a28f394ba5934e0787d6f600515db6ec366
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-24 23:57:15 +0200

    P3pitobanana

--- controllers/registration/success.php
+++ controllers/registration/success.php
@@ -1,40 +1,43 @@
 <?php
     require_once '../../models/database.php';
+    global $dbh;
     sendReservation(nouvelleReservation($_GET["q"], $_GET["i"], $_GET["p"], $_GET["email"],$_GET["date"], $_GET["heure"]));
         
 
 
     function novelleReservation($quantity, $idstripe, $unitprice, $email, $date, $heure): int //int =  retourne un int
     {
-    $query = $sdbh->prepare("SELECT count(id) as total FROM reservations");
-    $query->execute();
-    $result = $query->fetch(PDO::FETCH_ASSOC);
-    $rowid = $result["total"] + 1;
-    $attraction = getAttractionIdByStripeId($idstripe);
-    $total = $unitPrce * $quantity;
-    $query = $dbh -> prepare("INSERT INTO reservations (id, attractionid, montant, quantity, date, heure, email) VALUES(:id, :attraction,:montant, :quantity, :date, :heure, :email)");
-    $query -> bindParam(':id', $rowid);
-    $query -> bindParam(':attraction',$attraction);
-    $query -> bindParam(':montant', $total);
-    $query -> bindParam(':quantity', $quantity);
-    $query -> bindParam(':date', $date);
-    $query -> bindParam(':heure', $heure);
-    $query -> bindParam(':email', $email);
-    $query -> execute();
+        global $dbh;
+        $query = $dbh->prepare("SELECT count(id) as total FROM reservations");
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        $rowid = $result["total"] + 1;
+        $attraction = getAttractionIdByStripeId($idstripe);
+        $total = $unitPrce * $quantity;
+        $query = $dbh -> prepare("INSERT INTO reservations (id, attractionid, montant, quantity, date, heure, email) VALUES(:id, :attraction,:montant, :quantity, :date, :heure, :email)");
+        $query -> bindParam(':id', $rowid);
+        $query -> bindParam(':attraction',$attraction);
+        $query -> bindParam(':montant', $total);
+        $query -> bindParam(':quantity', $quantity);
+        $query -> bindParam(':date', $date);
+        $query -> bindParam(':heure', $heure);
+        $query -> bindParam(':email', $email);
+        $query -> execute();
 
-    return $rowid;
+        return $rowid;
     }
 
     function getAttractionIdByStripeId($stripeid): int 
     {
-    $query = $dbh -> prepare("SELECT id FROM attraction where idstripe = :idstripe");
-    $query -> bindParam(':idstripe', $stripeid);
-    $query -> execute();
-    $result = $query -> fetch();
-    return $result['id'];
+        global $dbh;
+        $query = $dbh -> prepare("SELECT id FROM attraction where idstripe = :idstripe");
+        $query -> bindParam(':idstripe', $stripeid);
+        $query -> execute();
+        $result = $query -> fetch();
+        return $result['id'];
     }
 
-    function sendreservation($id)
+    function sendreservation($id): void
     {
     //construire le pdf avec fpdf ou un truc du genre 
     //envoiyer par mail
