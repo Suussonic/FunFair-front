053840d164e811571b0d40000306b0587beb7449
Suussonic <abdoullahhad31@gmail.com>
2024-08-27 16:09:50 +0200

    Nav +fpdf

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,6 +1,7 @@
 <header>
     <?php
     if (isset($_SESSION['firstname'])) {
+        echo '<a class="Connexion" href="/">Compte</a>';
     } else {
         echo '<a class="Connexion" href="/login">Se Connecter</a>';
     }
