38a5ef610d3e85eca3c6e6b1a7d8cd93f61bd2cd
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-28 23:17:18 +0200

    captcha encore

--- views/back/captcha.view.php
+++ views/back/captcha.view.php
@@ -3,40 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Affichage du Captcha</title>
-    <style>
-        .admin-table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        .admin-table, .admin-table th, .admin-table td {
-            border: 1px solid black;
-        }
-        .admin-table th, .admin-table td {
-            padding: 8px;
-            text-align: left;
-        }
-        .admin-table th {
-            background-color: #007bff;
-            color: white;
-        }
-        .action-button {
-            background-color: #dc3545;
-            color: white;
-            border: none;
-            padding: 5px 10px;
-            cursor: pointer;
-            display: inline;
-        }
-        .action-button:hover {
-            background-color: #c82333;
-        }
-        .action-form {
-            display: inline;
-            margin: 0;
-            padding: 0;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/back/captcha.css">
+    <title>Captcha</title>
 </head>
 <body>
 
