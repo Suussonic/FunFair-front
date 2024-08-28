5afc769865d56655d4be22cde89054d3e554dc01
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-28 23:03:13 +0200

    correction nav

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,5 +1,7 @@
 <header>
     <?php
+    require_once('/models/Database.php');
+    session_start();
     if (isset($_SESSION['firstname'])) {
         echo '<a class="Connexion" href="/logout">deconnexion</a>';
         if ($_SESSION['role'] === 'admin') echo '<a class="Connexion" href="/back">back</a>';
