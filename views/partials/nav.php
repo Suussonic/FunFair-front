17f631e2515ea3589a79744f4e8b65745d13fc0e
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-28 22:51:17 +0200

    bouton

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -2,10 +2,9 @@
     <?php
     if (isset($_SESSION['firstname'])) {
         echo '<a class="Connexion" href="/logout">deconnexion</a>';
-        echo '<a class="Connexion" href="/back">back</a>';
+        if ($_SESSION['role'] === 'admin') echo '<a class="Connexion" href="/back">back</a>';
     } else {
         echo '<a class="Connexion" href="/login">Se Connecter</a>';
-        echo '<a class="Connexion" href="/back">back</a>';
     }
-   ?>
+    ?>
 </header>
