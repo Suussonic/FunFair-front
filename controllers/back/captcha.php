3fe5730c0b310a4b6b4f2746c27afa5b08be614e
Titouan <titouan.melchio@gmail.com>
2024-08-28 23:37:06 +0200

    capcha1.6

--- controllers/back/captcha.php
+++ controllers/back/captcha.php
@@ -1,6 +1,6 @@
 <?php
 include 'models/Database.php';
-include 'views/capcha.view.php';
+include 'views/back/capcha.view.php';
 if ($stmt->rowCount() > 0) {
     // Afficher les données de chaque ligne
     while ($row = $stmt->fetch()) {
