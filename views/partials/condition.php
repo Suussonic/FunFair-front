606fc5684b766a23dbdc983911f80c838527a072
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-29 00:21:48 +0200

    condition.css

--- views/partials/condition.php
+++ views/partials/condition.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="sUTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="../ASSET/CARDBINDEX V5.png" type="image/x-icon">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet" href="../CSS/condition.css">
+    <link rel="stylesheet" type="text/css" href="public/assets/css/condition.css">
     <title>Condition d'utilisation</title>
 </head>
 
