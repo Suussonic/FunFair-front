72020eb915d6b569f00ff7a068d98a5a5ecfe8c6
Titouan <titouan.melchio@gmail.com>
2024-08-28 22:56:12 +0200

    back

--- controllers/back/back.php
+++ controllers/back/back.php
@@ -1,4 +1,18 @@
 <?php
+include 'Database.php';
+if ($stmt->rowCount() > 0) {
+
+    while ($row = $stmt->fetch()) {
+        echo "<tr>
+            <td>" . htmlspecialchars($row["id"]) . "</td>
+            <td>" . htmlspecialchars($row["q"]) . "</td>
+            <td>" . htmlspecialchars($row["r"]) . "</td>
+        </tr>";
+    }
+} else {
+
+    echo "<tr><td colspan='4'>0 résultats</td></tr>";
+}
 
 
 
