b7e183be117b44afd82ecb041aa94bcb296b2f77
mancho1512 <borgesazerty@gmail.com>
2024-08-28 22:53:20 +0200

    Update back.view.php lien css

--- views/back/back.view.php
+++ views/back/back.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fun Fair - Back Office</title>
-    <link rel="stylesheet" href="back.css">
+    <link rel="stylesheet" href="public/assets/css/back.css">
 </head>
 <body>
     <div class="container">
