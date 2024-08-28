a4caad926cb10259521069e7199929e782344821
mancho1512 <borgesazerty@gmail.com>
2024-08-28 22:42:55 +0200

    Update back.view.php

--- views/back/back.view.php
+++ views/back/back.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fun Fair - Back Office</title>
-    <link rel="stylesheet" href="back-office.css">
+    <link rel="stylesheet" href="back.css">
 </head>
 <body>
     <div class="container">
