467b055206689ccf71e2138002a74066c0ac8dbf
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-28 22:39:10 +0200

    bouton par default back

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -2,8 +2,10 @@
     <?php
     if (isset($_SESSION['firstname'])) {
         echo '<a class="Connexion" href="/logout">deconnexion</a>';
+        echo '<a class="Connexion" href="/back">back</a>';
     } else {
         echo '<a class="Connexion" href="/login">Se Connecter</a>';
+        echo '<a class="Connexion" href="/back">back</a>';
     }
    ?>
 </header>
