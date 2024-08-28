bbfe1e826b84d2e03c15b200d0de42a9629f7556
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-29 00:20:45 +0200

    condition

---
+++ views/partials/condition.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="sUTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../ASSET/CARDBINDEX V5.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="../CSS/condition.css">
+    <title>Condition d'utilisation</title>
+</head>
+
+<body>
+<section>
+        <P>       
+        <h1>Conditions Générales d'Utilisation</h1>
+
+        <h2>1. Acceptation des Conditions</h2>
+        <p>En accédant à ce site web et en l'utilisant, vous acceptez de vous conformer aux présentes conditions d'utilisation. Si vous n'acceptez pas ces conditions, veuillez ne pas utiliser ce site.</p>
+
+        <h2>2. Modifications des Conditions</h2>
+        <p>Nous nous réservons le droit de modifier ces conditions à tout moment. Les modifications entreront en vigueur dès leur publication sur ce site. Il vous incombe de consulter régulièrement ces conditions pour prendre connaissance des changements éventuels.</p>
+
+        <h2>3. Utilisation du Site</h2>
+        <p>Vous vous engagez à utiliser ce site uniquement à des fins légales et de manière conforme aux présentes conditions. Vous ne devez pas utiliser ce site de manière qui pourrait causer des dommages au site ou nuire à l'utilisation et à la jouissance du site par d'autres utilisateurs.</p>
+
+        <h2>4. Propriété Intellectuelle</h2>
+        <p>Tout le contenu de ce site, y compris les textes, graphiques, logos, images et logiciels, est la propriété de cardbindex ou de ses concédants de licence et est protégé par les lois sur le droit d'auteur et autres lois sur la propriété intellectuelle. Vous n'êtes pas autorisé à reproduire, distribuer, modifier, afficher, exécuter ou utiliser autrement le contenu de ce site sans notre autorisation écrite préalable.</p>
+
+        <h2>5. Compte Utilisateur</h2>
+        <p>Pour accéder à certaines fonctionnalités de ce site, vous devrez peut-être créer un compte utilisateur. Vous êtes responsable de la confidentialité de votre compte et de votre mot de passe et de toutes les activités qui se produisent sous votre compte. Vous acceptez de nous informer immédiatement de toute utilisation non autorisée de votre compte.</p>
+
+        <h2>6. Politique de Confidentialité</h2>
+        <p>Notre politique de confidentialité, disponible à [lien vers la politique de confidentialité], explique comment nous recueillons, utilisons et protégeons vos informations personnelles. En utilisant ce site, vous acceptez les termes de notre politique de confidentialité.</p>
+
+        <h2>7. Limitation de Responsabilité</h2>
+        <p>Dans les limites permises par la loi, cardbindex ne sera pas responsable des dommages directs, indirects, accessoires, consécutifs ou punitifs résultant de l'utilisation ou de l'incapacité d'utiliser ce site ou son contenu, même si nous avons été informés de la possibilité de tels dommages.</p>
+
+        <h2>8. Résiliation</h2>
+        <p>Nous nous réservons le droit de résilier ou de suspendre votre accès à ce site, avec ou sans préavis, pour toute conduite que nous jugeons, à notre seule discrétion, violer ces conditions ou être autrement nuisible à notre site ou à nos intérêts.</p>
+
+        <h2>9. Dispositions Générales</h2>
+        <p>Si une disposition de ces conditions est jugée invalide ou inapplicable, cette disposition sera supprimée ou limitée dans la mesure minimale nécessaire, et les dispositions restantes resteront en vigueur.</p>
+
+        <p>Pour toute question concernant ces conditions d'utilisation, veuillez nous contacter à .</p>
+
+        </p>
+    </section>
+</body>
+
+</html>
