2ccccf9f9eda882f850b1374ab3e2e2ef63af32a
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-08-28 23:31:05 +0200

    remise a zero

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,31 +1,12 @@
-<?php
-// Inclure le fichier de connexion à la base de données
-
-include 'models/Database.php';
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-if (isset($_SESSION['firstname'])) {
-    // Récupérer le rôle de l'utilisateur depuis la base de données
-    $id = $_SESSION['id']; // Supposons que l'ID de l'utilisateur est stocké dans $_SESSION['id']
-    $sql = "SELECT role FROM users WHERE id = $id";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $role = $row['role'];
+<header>
+    <?php
+    if (isset($_SESSION['firstname'])) {
+        echo '<a class="Connexion" href="/logout">deconnexion</a>';
+        echo '<a class="Connexion" href="/back">Back temporaire</a>';
+        if ($_SESSION['role'] === 'admin') echo '<a class="Connexion" href="/back">back</a>';
+    } else {
+        echo '<a class="Connexion" href="/login">Se Connecter</a>';
+        echo '<a class="Connexion" href="/back">autre back temporaire</a>';
     }
-
-    // Affichage des boutons selon le rôle
-    echo '<a class="Connexion" href="/logout">deconnexion</a>';
-    echo '<a class="Connexion" href="/back">back</a>';
-    if ($role === 'admin') {
-        echo '<a class="Connexion" href="/admin">Admin Panel</a>';
-    }
-} else {
-    echo '<a class="Connexion" href="/login">Se Connecter</a>';
-    echo '<a class="Connexion" href="/back">back</a>';
-}
-?>
+    ?>
+</header>
