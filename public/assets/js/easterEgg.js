9e63010d735f2399c9bf7c505e96bcfc9695081e
Nathan <90607880+ngerault@users.noreply.github.com>
2024-08-28 11:05:50 +0100

    Create easterEgg.js

---
+++ public/assets/js/easterEgg.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Séquence du Konami Code
+    const konamiCode = [
+        "ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown",
+        "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight",
+        "b", "a", "Enter"
+    ];
+
+    let konamiCodePosition = 0;
+
+    // Fonction pour télécharger l'image
+    function downloadImage(url) {
+        console.log("Konami Code entered! Downloading image...");
+        let a = document.createElement("a");
+        a.href = url;
+        a.download = "konami_image.jpg";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+
+    // Écouteur d'événements pour les touches du clavier
+    document.addEventListener("keydown", function(event) {
+        if (event.key === konamiCode[konamiCodePosition]) {
+            konamiCodePosition++;
+            if (konamiCodePosition === konamiCode.length) {
+                // URL de l'image à télécharger
+                const imageUrl = "../ASSET/LOGONVMAX.png";
+                const imageUrl2 = "../ASSET/EASTEREGG2.png";
+                new Audio('../ASSET/SONICRING.mp3').play();
+                downloadImage(imageUrl);
+                downloadImage(imageUrl2);
+                konamiCodePosition = 0;
+            }
+        } else {
+            konamiCodePosition = 0;
+        }
+    });
+});
