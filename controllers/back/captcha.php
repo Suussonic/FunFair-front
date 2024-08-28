b555428e6966cc1fbec652ab31d23cf5667ba9d0
Titouan <titouan.melchio@gmail.com>
2024-08-28 23:45:18 +0200

    capcha1.9

--- controllers/back/captcha.php
+++ controllers/back/captcha.php
@@ -2,15 +2,8 @@
 include 'models/Database.php';
 include 'views/back/captcha.view.php';
 
-if (isset($_POST['delete_id'])) {
-    $delete_id = $_POST['delete_id'];
-    $delete_sql = "DELETE FROM captcha WHERE id = :id";
-    $stmt = $dbh->prepare($delete_sql);
-    $stmt->execute([':id' => $delete_id]);
-
-header("Location: " . $_SERVER['PHP_SELF']);
-exit;
-}
+$sql = "SELECT id, q, r FROM captcha";
+$stmt = $dbh->query($sql);
 
 if ($stmt->rowCount() > 0) {
 
