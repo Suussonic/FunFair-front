e4e7b0cff8bd536a21aa1a802d88cff6e70fed20
mancho1512 <borgesazerty@gmail.com>
2024-08-31 10:46:59 +0200

    Update index.view.php rajout bouton

--- views/index.view.php
+++ views/index.view.php
@@ -14,6 +14,16 @@
     <header>
         <?php include 'partials/nav.php'; ?>
     </header>
+     <div style="flex: 1; min-width: 200px; padding: 10px;">
+                <h3 style="color: #ff6f61; margin-bottom: 15px;">Liens rapides</h3>
+                <ul style="list-style: none; padding: 0;">
+                    <li><a href="HTML/attractions.html" style="color: #f0f0f0; text-decoration: none;">Attractions</a></li>
+                    <li><a href="HTML/horaires.html" style="color: #f0f0f0; text-decoration: none;">Horaires</a></li>
+                    <li><a href="HTML/billets.html" style="color: #f0f0f0; text-decoration: none;">Billets</a></li>
+                    <li><a href="HTML/avis.html" style="color: #f0f0f0; text-decoration: none;">Laissez un Avis</a></li>
+                    <li><a href="HTML/contact.html" style="color: #f0f0f0; text-decoration: none;">Contact</a></li>
+                </ul>
+            </div>
     <div>
         <h1>Bienvenue à Fun Fair</h1>
         <p>Le parc d'attractions où le plaisir ne s'arrête jamais !</p>
