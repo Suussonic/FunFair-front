fd4885dc194e87480dd2478c0e00f620837333e0
mancho1512 <borgesazerty@gmail.com>
2024-09-01 22:51:31 +0200

    Update mailer.phpcommentaire

--- controllers/mailer.php
+++ controllers/mailer.php
@@ -11,21 +11,20 @@ function sendMail($dbh, $email, $objet, $body) {
         ]);
         $response = $query->fetchAll();
         $username = $response[0]['firstname'];
-        // Tentative de création d’une nouvelle instance de la classe PHPMailer, avec les exceptions activées
+        
         $mail = new PHPMailer (true);
         $mail = initMailer($mail);
-        // Expéditeur
+      
         $mail->setFrom('funfair-no-reply@echovps.fr', 'No reply - Fun Fair'); 
-        // Destinataire dont le nom peut également être indiqué en option
         $mail->addAddress($email, $username);
 
         $mail->isHTML(true);
-        // Objet
+      
         $mail->Subject = $objet;
-        // HTML-Content
+      
         $mail->Body = $body;
 
-        // Ajouter une pièce jointe
+     
         $mail->CharSet = 'UTF-8';
         $mail->Encoding = 'base64';
         $mail->send();
@@ -37,10 +36,10 @@ function sendMail($dbh, $email, $objet, $body) {
 
 function initMailer($mail) {
     $mail->isSMTP();
-    $mail->Host = 'live.smtp.mailtrap.io'; // Your SMTP server
+    $mail->Host = 'live.smtp.mailtrap.io';
     $mail->SMTPAuth = true;
-    $mail->Username = 'api'; // Your Mailtrap username
-    $mail->Password = '5e1169d1cf99606bbbb2480f5177958f'; // Your Mailtrap password
+    $mail->Username = 'api';
+    $mail->Password = '5e1169d1cf99606bbbb2480f5177958f'; 
     $mail->Port = 587;
     return $mail;
 }
