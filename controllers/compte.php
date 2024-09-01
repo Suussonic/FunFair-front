25896d386cfb3b3521117f87c85ac234f0085163
Titouan <titouan.melchio@gmail.com>
2024-09-01 22:00:58 +0200

    compte css

--- controllers/compte.php
+++ controllers/compte.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/partials.css">
+    <link rel="stylesheet" href="/public/assets/css/compte.css">
     <title>Mon compte</title>
 </head>
 <body>
