41443a19446850a15f6a26134879e819d3cfc8ee
Crokmoo <98886828+Crokmooo@users.noreply.github.com>
2024-09-01 23:42:42 +0200

    Update process_drawing.php

--- controllers/concours/process_drawing.php
+++ controllers/concours/process_drawing.php
@@ -5,6 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once('../../models/Database.php');
+session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['canvasData']) && !empty($_POST['canvasData'])) {
