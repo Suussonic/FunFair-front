b77dcfb49f58edd8b4f4589797dba90769396ff9
Titouan <titouan.melchio@gmail.com>
2024-09-01 15:15:25 +0200

    Logs

---
+++ controllers/logs.php
@@ -0,0 +1,35 @@
+<?php
+include('Database.php');
+
+function insert_logs($action) {
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    global $dbh;
+
+    if (!$dbh) {
+        die('Connexion à la base de données échouée.');
+    }
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $date_now = date('Y-m-d H:i:s');
+    $firstname = isset($_SESSION['firstname']) ? $_SESSION['firstname'] : '';
+    $email = isset($_SESSION['user']['email']) ? $_SESSION['user']['email'] : '';
+
+    $logs_req = 'INSERT INTO logs (action, ip, date, firstname, email) VALUES (:action, :ip, :date, :firstname, :email)';
+    $logs_query = $dbh->prepare($logs_req);
+
+    try {
+        $logs_query->execute([
+            ':action' => $action,
+            ':ip' => $ip,
+            ':date' => $date_now,
+            ':firstname' => $firstname,
+            ':email' => $email
+        ]);
+    } catch (PDOException $e) {
+        die('Erreur lors de l\'insertion du log : ' . $e->getMessage());
+    }
+}
+?>
