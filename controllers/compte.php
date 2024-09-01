ecf41f48e75dc4529c25b1cf140aed37e20ef04b
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-09-01 22:02:34 +0200

    compte+

--- controllers/compte.php
+++ controllers/compte.php
@@ -1,39 +1,117 @@
+<?php
+global $dbh;
+session_start();
+include_once('models/Database.php');
+
+if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+    $editUserSql = '
+        UPDATE users
+        SET firstname = :firstname,
+            lastname = :lastname,
+            email = :email,
+            gender = :gender
+        WHERE id = :id
+    ';
+
+    $preparedEditUser = $dbh->prepare($editUserSql);
+    $preparedEditUser->execute([
+        'firstname' => $_POST['firstname'],
+        'lastname' => $_POST['lastname'],
+        'email' => $_POST['email'],
+        'gender' => $_POST['gender'],
+        'id' => $_SESSION['id']
+    ]);
+    $_SESSION['firstname'] = $_POST['firstname'];
+    $_SESSION['lastname'] = $_POST['lastname'];
+}
+
+
+$getUser = "SELECT id, firstname, lastname, email, gender FROM users WHERE id = :id";
+
+$preparedGetUser = $dbh->prepare($getUser);
+$preparedGetUser->execute([
+        'id' => $_SESSION['id']
+]);
+
+$user = $preparedGetUser->fetch();
+
+?>
+
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/public/assets/css/partials.css">
-    <link rel="stylesheet" href="/public/assets/css/compte.css">
     <title>Mon compte</title>
 </head>
 <body>
-    <h1>Modifier les informations de votre compte</h1>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="../register/form.php">Créer compte</a></li>
+                <li><a href="../login/loginForm.php">Login</a></li>
+                <li><a href="../account/account.php">Mon compte</a></li>
+                <li><a href="../logout.php">Se deconnecter</a></li>
+            </ul>
+
+        </nav>
+    </header>
+
+    <h1>Bienvenue <?php echo $_SESSION['firstname'] . ' ' . $_SESSION['lastname'] ?></h1>
 
     <form action="" method="POST">
+
         <div>
-            <label for="firstname">Prénom:</label>
-            <input type="text" id="firstname" name="firstname" value="<?php echo htmlspecialchars($_SESSION['firstname'], ENT_QUOTES); ?>" required>
+            <label for="firstname">Prenom</label>
+            <input
+                    id="firstname"
+                    type="text"
+                    name="firstname"
+                    placeholder="Prenom"
+                    value="<?php echo $user['firstname'] ?>"
+            >
         </div>
+        <!--  NOM  -->
         <div>
-            <label for="lastname">Nom:</label>
-            <input type="text" id="lastname" name="lastname" value="<?php echo htmlspecialchars($_SESSION['lastname'], ENT_QUOTES); ?>" required>
+            <label for="lastname">Nom</label>
+            <input id="lastname" type="text" name="lastname" value="<?php echo $user['lastname'] ?>">
         </div>
+        <!--  EMAIL  -->
         <div>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($_SESSION['email'], ENT_QUOTES); ?>" required>
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" value="<?php echo $user['email'] ?>">
         </div>
+        <!--  GENRE (RADIO button)  -->
         <div>
-            <label for="gender">Genre:</label>
-            <select id="gender" name="gender" required>
-                <option value="male" <?php echo $_SESSION['gender'] === 'male' ? 'selected' : ''; ?>>Homme</option>
-                <option value="female" <?php echo $_SESSION['gender'] === 'female' ? 'selected' : ''; ?>>Femme</option>
-                <option value="other" <?php echo $_SESSION['gender'] === 'other' ? 'selected' : ''; ?>>Autre</option>
-            </select>
+            <label for="man">Homme</label>
+            <input
+                    id="man"
+                    type="radio"
+                    name="gender"
+                    value="man"
+                <?php echo $user['gender'] === "man" ? 'checked' : '' ?>
+            >
+
+            <label for="woman">Femme</label>
+            <input
+                    id="woman"
+                    type="radio"
+                    name="gender"
+                    value="woman"
+                <?php echo $user['gender'] === "woman" ? 'checked' : '' ?>
+            >
+
+            <label for="other">Autre</label>
+            <input
+                    id="other"
+                    type="radio"
+                    name="gender"
+                    value="other"
+                <?php echo $user['gender'] === "other" ? 'checked' : '' ?>
+            >
         </div>
-        <div>
-            <button type="submit">Mettre à jour</button>
-        </div>
+
+        <input type="submit" value="Modifier">
+
     </form>
 </body>
 </html>
