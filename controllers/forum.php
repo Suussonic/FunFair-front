1f23aad0782d7a548f1a4fce4aabb00c509b580d
Nathan <nathounng@gmail.com>
2024-09-01 23:01:29 +0200

    drole de degaine

--- controllers/forum.php
+++ controllers/forum.php
@@ -24,7 +24,7 @@ $questions = $stmt->fetchAll();
 <ul>
     <?php foreach ($questions as $question): ?>
         <li>
-            <a href="question.php?id=<?= $question['id'] ?>">
+            <a href="fquestion.php?id=<?= $question['id'] ?>">
                 <strong><?= htmlspecialchars($question['title']) ?></strong><br>
                 <?= htmlspecialchars($question['description']) ?><br>
                 <small>Posté par <?= htmlspecialchars($question['name_author']) ?> le <?= $question['date_publication'] ?></small>
