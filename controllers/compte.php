365201eae621a12f049d60f13dd41f00104288b7
Titouan <titouan.melchio@gmail.com>
2024-09-01 19:14:44 +0200

    compte 2

--- controllers/compte.php
+++ controllers/compte.php
@@ -1,9 +1,9 @@
 <?php
 global $dbh;
 session_start();
-include_once('../db.php');
+include_once('../Database.php');
 
-if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $editUserSql = '
         UPDATE users
         SET firstname = :firstname,
@@ -21,22 +21,23 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
         'gender' => $_POST['gender'],
         'id' => $_SESSION['userId']
     ]);
+
+    // Update session variables after successful update
     $_SESSION['firstname'] = $_POST['firstname'];
     $_SESSION['lastname'] = $_POST['lastname'];
 }
 
-
+// Retrieve the user's current data
 $getUser = "SELECT id, firstname, lastname, email, gender FROM users WHERE id = :id";
 
 $preparedGetUser = $dbh->prepare($getUser);
 $preparedGetUser->execute([
-        'id' => $_SESSION['userId']
+    'id' => $_SESSION['userId']
 ]);
 
 $user = $preparedGetUser->fetch();
 
-
+// Include the view that displays the user's account information
 require 'views/compte.view.php';
 
 ?>
-
